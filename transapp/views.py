ee032c2444012bf2f963d44d346847b8fe52bb5a
hackerton2003 <esbondarlington@gmail.com>
2025-12-26 08:50:34 +0300

    First Commit

---
+++ transapp/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from translate import Translator
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    if request.method == 'POST':
+        text = request.POST['translate']
+        language = request.POST['language']
+        translator = Translator(to_lang = language)
+        translation = translator.translate(text)
+        
+        return HttpResponse(translation)
+        
+    return render(request, 'index.html')
